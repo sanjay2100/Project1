170449d62e65c392ba09c4b88d5f00fd8761d34c
sanjay <sanjaymanikandan21@gmail.com>
2023-02-10 23:23:27 +0530

    added category

--- antartica/src/App.js
+++ antartica/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import{useState} from "react";
-import Nav from './components';
+import {Nav,Category} from './components';
 import ScrollAnimation from 'react-animate-on-scroll';
 import "animate.css/animate.min.css";
 import Login from './Login';
@@ -15,7 +15,7 @@ function App() {
 
   //js for changing theme
   const [Theme, NewTheme]=useState("Light")
-  const [link,setLink]=useState("")
+  const [link,setLink]=useState("https://cdn.pixabay.com/photo/2013/07/12/14/53/cart-148964_960_720.png")
 
   const Click=()=>{
     if (Theme==="Dark"){
@@ -39,7 +39,7 @@ function App() {
      <div className="header" id='head'>
       <Nav/>
       <h2>Antartica</h2>
-      <button onClick={()=>Click()}>theme</button>
+      <button className='theBut' onClick={()=>Click()}>theme</button>
       <Login open={model} close={setModel}/>
       <div className='Loginbutton'>
       
@@ -48,12 +48,17 @@ function App() {
       </div>
       <img className='image' src={link}/>
      </div>
-     <div id='body'>
-      <div className='FASHION'>
-      <img className='fashion' src='https://images.pexels.com/photos/8108334/pexels-photo-8108334.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'/>
-      <img className='fashion' src='https://images.pexels.com/photos/7061960/pexels-photo-7061960.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'/>
-      <h2 className='unleash'>Unleash the fashion freak in you</h2>
-     </div>
+     <div id='body' className='body'>
+     <div className='FASHION'>
+      <img className='fashion' src='https://www.apple.com/newsroom/images/product/iphone/standard/Apple-iPhone-14-iPhone-14-Plus-hero-220907_Full-Bleed-Image.jpg.large.jpg'/>
+      <img className='fashion' src='https://www.techspot.com/articles-info/2295/images/2021-07-20-image-2.jpg'/>
+      <h2 className='trend'>Explore latest Technology</h2>
+      </div>
+     <Category/>
+     
+  
+     
+     
      </div>
     </div>
   );
--- antartica/src/components.js
+++ antartica/src/components.js
@@ -14,5 +14,51 @@ const Nav=()=>{
         </div>
     )
 }
+const Category=()=>{
+    return(
+        <div className='category'>
+      <div className='container'>
+        <img className='catimg' src="https://images.pexels.com/photos/325153/pexels-photo-325153.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+        <h3 className='des'>electronics</h3>
+      </div>
+      <div className='container'>
+        <img className='catimg' src="https://images.pexels.com/photos/1536619/pexels-photo-1536619.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+        <h3 className='des'>fashion</h3>
+      </div>
+      <div className='container'>
+        <img className='catimg' src="https://images.pexels.com/photos/1099816/pexels-photo-1099816.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+        <h3 className='des'>Decoration</h3>
+      </div>
+      <div className='container'>
+        <img className='catimg' src="https://images.pexels.com/photos/868110/pexels-photo-868110.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+        <h3 className='des'>Grocery</h3>
+      </div>
+      <div className='container'>
+        <img className='catimg' src="https://images.pexels.com/photos/1105019/pexels-photo-1105019.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+        <h3 className='des'>Gardening</h3>
+      </div>
+      <div className='container'>
+        <img className='catimg' src="https://images.pexels.com/photos/4934174/pexels-photo-4934174.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+        <h3 className='des'>Toys</h3>
+      </div>
+      <div className='container'>
+        <img className='catimg' src="https://cdn.pixabay.com/photo/2018/07/22/08/49/tennis-3554019_960_720.jpg"/>
+        <h3 className='des'>Sports</h3>
+      </div>
+      <div className='container'>
+        <img className='catimg' src="https://cdn.pixabay.com/photo/2018/02/11/22/21/paper-3146952_960_720.jpg"/>
+        <h3 className='des'>Stationary</h3>
+      </div>
+      <div className='container'>
+        <img className='catimg' src="https://cdn.pixabay.com/photo/2015/02/03/02/03/car-621820_960_720.jpg"/>
+        <h3 className='des'>Spares</h3>
+      </div>
+      <div className='container'>
+        <img className='catimg' src="https://images.pexels.com/photos/5910956/pexels-photo-5910956.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+        <h3 className='des'>Medicine</h3>
+      </div>
+     </div>
+    )
+}
 
-export default Nav
+export  {Nav,Category}
